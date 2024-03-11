4364764e69f5353532978e31ccfed73236479f2b
Tymofii Skopenko <youmustdeth@gmail.com>
2024-03-11 12:08:01 +0200

    add urls & views for each use case

--- core/views.py
+++ core/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from core.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 5
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('core:task-list')
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('core:task-list')
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy('core:task-list')
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = 'tag_list'
+    template_name = 'core/tag_list.html'
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = '__all__'
+    success_url = reverse_lazy("core:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = '__all__'
+    success_url = reverse_lazy("core:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("core:tag-list")
--- to_do/urls.py
+++ to_do/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path("", include("core.urls", namespace="core")),
+    path("", include("core.urls", namespace="core")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
